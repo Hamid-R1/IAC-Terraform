9dec89f41d68fb515f6929ed02cf1a35a261aa87
Hamid-R1 <112654341+Hamid-R1@users.noreply.github.com>
2023-10-31 17:58:27 +0530

    Update 5-deploy_website.sh

--- 6-deploy-basic-website/5-deploy_website.sh
+++ 6-deploy-basic-website/5-deploy_website.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 # System Updates
-sudo su -
-yum -y update
+sudo yum -y update
 
 # Install Apache Web Server
-yum install -y httpd
+sudo yum install -y httpd
 
 # Start & enable Apache Web Server
-systemctl start httpd
-systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
 # Install git & clone 'html-css-js-code' from github
-yum install -y git
+sudo yum install -y git
 mkdir source-code
 cd source-code
-git clone https://github.com/Hamid-R1/basic_profile_website.git
+sudo git clone https://github.com/Hamid-R1/basic_profile_website.git
 
 # Configure & setup 'Application' to this path '/var/www/html/'
 cd basic_profile_website/DevFolio/
-cp -r * /var/www/html/
+sudo cp -r * /var/www/html/
